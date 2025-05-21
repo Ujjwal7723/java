fca925b8a9163c945cec3d5cb847debf07fc782d
ujjwal jha <jhaujjwal483@gmail.com>
2025-05-21 07:56:51 +0545

    Updated 19 lines of code

---
+++ Bicte5Th.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Bicte5Th 
+{
+
+	
+	    public static void main(String[] args) 
+	    {
+	        Scanner input = new Scanner(System.in);
+
+	        System.out.print("Enter first number: ");
+	        double a = input.nextDouble();
+
+	        System.out.print("Enter second number: ");
+	        double b = input.nextDouble();
+
+	        double sum = a + b;
+	        System.out.println("The sum is: " + sum);
+	    }
+	}
